50f716f9eba2c1599ae4b3e8d242e5f33c5f1385
Chad Ostrowski <hi@chadoh.com>
2018-02-26 19:59:06 -0500

    GraphQL: Use `graphql` HOC from Apollo

---
+++ graphql-frontend/src/components/LinkList.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
+import Link from './Link'
+
+let LinkList = ({feedQuery}) => {
+  if (feedQuery && feedQuery.loading) {
+    return <div>Loading</div>
+  }
+
+  if (feedQuery && feedQuery.error) {
+    return <div>Error: {feedQuery.error}</div>
+  }
+
+  const linksToRender = feedQuery.feed.links
+
+  return (
+    <div>{linksToRender.map(link => <Link key={link.id} link={link} />)}</div>
+  )
+}
+
+const FEED_QUERY = gql`
+  query FeedQuery {
+    feed {
+      links {
+        id
+        createdAt
+        url
+        description
+      }
+    }
+  }
+`
+
+export default graphql(FEED_QUERY, { name: 'feedQuery' })(LinkList)
