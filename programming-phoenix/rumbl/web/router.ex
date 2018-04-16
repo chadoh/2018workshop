146b62ae0928df09f1264576c17329010cde0148
Chad Ostrowski <hi@chadoh.com>
2018-04-16 07:39:15 -0400

    programming-phoenix: aesthetic upgrades

--- programming-phoenix/rumbl/web/router.ex
+++ programming-phoenix/rumbl/web/router.ex
@@ -15,7 +15,7 @@ defmodule Rumbl.Router do
   end
 
   scope "/", Rumbl do
-    pipe_through :browser # Use the default browser stack
+    pipe_through :browser
     get "/", PageController, :index
     resources "/users", UserController, only: [:index, :show, :new, :create]
     resources "/sessions", SessionController, only: [:new, :create, :delete]
