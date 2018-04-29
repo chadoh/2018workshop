a2b8c6a2af049b2c2dc07479242d7c24eed69486
Chad Ostrowski <hi@chadoh.com>
2018-04-29 11:07:30 -0400

    programming-phoenix: remove deprecated Dict.merge
    Solution found on https://stackoverflow.com/a/41650633/249801
    
    Switching to Enum.into required reversing the order of the arguments,
    since we want to return a Map, and we are passing Keyword Lists as
    `attrs` in various tests.

--- programming-phoenix/rumbl/test/support/test_helpers.ex
+++ programming-phoenix/rumbl/test/support/test_helpers.ex
@@ -2,11 +2,11 @@ defmodule Rumbl.TestHelpers do
   alias Rumbl.Repo
 
   def insert_user(attrs \\ %{}) do
-    changes = Dict.merge(%{
+    changes = Enum.into(attrs, %{
       name: "Some User",
       username: "user#{Base.encode16(:crypto.strong_rand_bytes(8))}",
       password: "supersecret",
-    }, attrs)
+    })
 
     %Rumbl.User{}
     |> Rumbl.User.registration_changeset(changes)
