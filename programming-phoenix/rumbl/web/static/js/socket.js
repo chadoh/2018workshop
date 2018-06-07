caa633e8bba783deb08224936dfbca4a3e9dbe6d
Chad Ostrowski <hi@chadoh.com>
2018-06-06 22:44:55 -0400

    programming-phoenix/ch10: authenticate socket

--- programming-phoenix/rumbl/web/static/js/socket.js
+++ programming-phoenix/rumbl/web/static/js/socket.js
@@ -5,7 +5,7 @@
 // and connect at the socket path in "lib/my_app/endpoint.ex":
 import {Socket} from "phoenix"
 
-let socket = new Socket("/socket", {
+const socket = new Socket("/socket", {
   params: {token: window.userToken},
   logger: (kind, msg, data) => { console.log(`${kind}: ${msg}`, data)}
 })
